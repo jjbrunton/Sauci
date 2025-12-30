88543c777507c48df74ccbc136c257288da63d2d
Jamie Brunton <jamie.brunton@dootrix.com>
2025-12-30 17:57:06 +0000

    Update

--- apps/admin/vite.config.ts
+++ apps/admin/vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
     server: {
         host: true,
         port: 3001,
+        allowedHosts: ['manage.sauci.app'],
         watch: {
             usePolling: true,
             interval: 100,
